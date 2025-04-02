d0304f387149f3480d8d93a8812f9f1451229efc
Mert Özbolat <122216794+Mert-Ozbolat@users.noreply.github.com>
2025-04-02 16:57:47 +0300

    update

--- backend/methods/post.js
+++ backend/methods/post.js
@@ -1,17 +1,45 @@
-const bodyParser = require("../utils/bodyParser")
+const bodyParser = require("../utils/bodyParser");
+const crypto = require('crypto')
+const fs = require('fs')
+
+const keys = [
+    "title",
+    "year",
+    "rating",
+    "description",
+    "language",
+    "director",
+];
 
 const postRequest = async (req, res) => {
-    if (req.url === "api/movies/") {
-        const body = await bodyParser(req)
-        console.log("gelen body", body)
-        res.end('film olusturuldu')
+    if (req.url === "/api/movies") {
+        const body = await bodyParser(req);
+
+        if (
+            keys.some((key) => !body[key]) ||
+            !body.genre.length > 0 ||
+            !body.cast.length > 0
+        ) {
+            res.writeHead(404)
+            res.end("Lutfen zounlu alan butun alanlari tanimlayin");
+            return;
+        }
+
+        body.id = crypto.randomUUID();
+
+        let data = fs.readFileSync('./data/movies.json', 'utf-8')
+        data = JSON.parse(data)
+
+        data.push(body)
+
+        fs.writeFileSync('./data/movies.json', body)
+
+
+
+        res.end("film olusturuldu");
     } else {
         res.writeHead(404);
-        res.end("Gecersiz yls istek atıldı")
+        res.end("Gecersiz yola istek");
     }
-
-}
-
-
-
-module.exports = postRequest
+};
+module.exports = postRequest;
